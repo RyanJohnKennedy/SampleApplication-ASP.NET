9223249507d06b781373920e176c3739b53a1805
Ryan Kennedy <ryankennedy@Ryans-MacBook-Air.local>
2024-04-19 14:33:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# SampleApplication-ASP.NET
---
+++ SampleAplication/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SampleAplication.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
